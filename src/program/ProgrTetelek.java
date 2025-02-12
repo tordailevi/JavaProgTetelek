140249459a6ae0c1fef13a1f72bbeaa6f6c8363f
tordailevi <tordailevigmail.com>
2025-02-12 08:11:32 +0100

    kész

--- src/program/ProgrTetelek.java
+++ src/program/ProgrTetelek.java
@@ -9,6 +9,7 @@ public class ProgrTetelek {
         int hossz = sorozat.length;
         
     }
+    
     public static int osszegzes(int [] sorozat, int hossz) {
             int osszeg = 0;
             for (int i = 0; i < hossz; i++) {
@@ -16,7 +17,67 @@ public class ProgrTetelek {
             }
             return osszeg;
     }
-        
-        
+
+    public static int megszamlalas(int[] sorozat) {
+            int osszeg = 0;
+            for (int i = 0; i < sorozat.length; i++) {
+                if (sorozat[i] % 2 == 1) {
+                    osszeg++;
+                }
+            }
+            return osszeg;
+        }
+
+    public static int minKivalasztas(int[] sorozat) {
+        int minIndex = 0;
+        for (int i = 1; i < sorozat.length; i++) {
+            if (sorozat[i] < sorozat[minIndex]) {
+                minIndex = i;
+            }
+        }
+        return minIndex;
+    }
+
+    public static int maxKivalasztas(int[] sorozat) {
+        int maxIndex = 0;
+        for (int i = 1; i < sorozat.length; i++) {
+            if (sorozat[i] > sorozat[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
+
+    public static int kivalasztas(int[] sorozat) {
+        int i = 0;
+        while (i < sorozat.length && !(sorozat[i] > 5)) {
+            i++;
+        }
+        return i;
+    }
+
+    public static boolean eldontesEgy(int[] sorozat) {
+        int i = 0;
+        while (i < sorozat.length && !(sorozat[i] % 2 == 0 && sorozat[i] > 4)) {
+            i++;
+        }
+        return i < sorozat.length;
+    }
+
+    public static boolean eldontesMind(int[] sorozat) {
+        int i = 0;
+        while (i < sorozat.length && sorozat[i] > 5) {
+            i++;
+        }
+        return i >= sorozat.length;
+    }
+
+    public static int linearisKereses(int[] sorozat) {
+        int i = 0;
+        while (i < sorozat.length && sorozat[i] != 0) {
+            i++;
+        }
+        return (i < sorozat.length) ? i : -1;
+    }
 }
 
